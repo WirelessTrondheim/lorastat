0d0bdb514e35e05cc3f87c68bbe93eea333e81d1
Arne Sund <arnels@stortinget.samfundet.no>
2017-07-26 18:22:08 +0200

    Init with basic pg_connection

---
+++ conf.php
@@ -0,0 +1,8 @@
+<?php
+
+   $host        = "host = 127.0.0.1";
+   $port        = "port = 5434";
+   $dbname      = "dbname = lorastatdb";
+   $credentials = "user = lorastat password=fancypw";
+
+?>
